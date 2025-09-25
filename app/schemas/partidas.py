89baad93b3d3ef0392b96fee08bcb8797050d12e
DiogePuig <diogenes.puig@mi.unc.edu.ar>
2025-09-25 14:49:32 -0300

    SCRUM-14: partida schemas modificado

--- app/schemas/partidas.py
+++ app/schemas/partidas.py
@@ -1,6 +1,8 @@
 # Aca defino modelos de datos para partidas
 from pydantic import BaseModel
 from datetime import datetime
+from typing import List
+
 
 """
 Nota: estas clases en endpoints hacen que se verifiquen solos
@@ -8,19 +10,33 @@ los tipos.
 El formato de fechas es el de ISO 8601 (YYYY-MM-DDThh:mm:ss),
 el preferido en la mayoria de las app web
 """
+class Jugador(BaseModel): # Deberiamos pasar esto a un archivo schemas/jugador.py ? 
+    id_jugador: int
+    nombre: str
+    fecha_nacimiento: datetime
+    # Falta el turno ?
+    # Falta avatar ? 
+    
+    class Config: 
+        orm_mode = True
+
 class PartidaCreada(BaseModel):
     jugador_creador: str
     fecha_nac: datetime
     minimo: int
     maximo: int
-   
-class Jugador(BaseModel):
-    nombre: str
-    fecha_nac: datetime
 
 class Partida(BaseModel):
     # Esquema de los datos de partida que se envian a los usuarios
-    ID: int
+    id_partida: int
     minimo: int
     maximo: int
-    cantidad: int
+    estado: str
+    cantidad_jugadores: int
+    turno_actual: int
+    jugadores: List[Jugador] = []
+    
+    class Config:
+        orm_mode = True
+
+
