8624573f5b651c6257cfe6ec2d2996d9e3d522b4
DiogePuig <diogenes.puig@mi.unc.edu.ar>
2025-09-25 16:30:29 -0300

    SCRUM-14: add new endpoints

--- app/routers/partidas.py
+++ app/routers/partidas.py
@@ -11,8 +11,9 @@ from app.db.models.jugadores_models import Jugador as JugadorModel
 from sqlalchemy.orm import Session
 
 partida_router= APIRouter()
+
 """
-endpoints usados para probar cosas (adaptar y usar luego los que son)
+#endpoints usados para probar cosas (adaptar y usar luego los que son)
 @partida_router.get(path="/partidas")
 async def listar_partidas() -> List[PartidaSchema]:
     # Aca se define la logica para listar partidas no empezadas,
@@ -66,11 +67,62 @@ async def crear_partida(partida: PartidaCreada):
     "id_partida": partida_id,
     "id_jugador_creador": creador_id
     }
-
 """
-@partida_router.get("/{partida_id}", response_model=PartidaSchema)
+
+@partida_router.get("partidas/{partida_id}", response_model=PartidaSchema)
 def obtener_partida(partida_id: int, db: Session = Depends(get_db)):
     partida = db.query(PartidaModel).filter(PartidaModel.id_partida == partida_id).first()
     if not partida:
         raise HTTPException(status_code=404, detail="Partida no encontrada")
     return partida
+
+@partida_router.patch("partidas/{partida_id}/iniciar", response_model=None , status_code=status.HTTP_200_OK)
+def iniciar_partida(partida_id:int, data: dict, db: Session = Depends(get_db)):
+    partida = db.query(PartidaModel).filter(PartidaModel.id_partida == partida_id).first()
+
+    if not partida:
+        raise HTTPException(status_code=404, detail="Partida no encontrada")
+    
+    if partida.estado == EstadoPartida.en_espera:
+        partida.estado = EstadoPartida.en_juego  
+        # si quiero cambiar a un estado q me llega por usuario deberia usar
+        # el dict q me llega leerlo y modificar el estado.
+    elif partida.estado == EstadoPartida.en_juego:
+        raise HTTPException(status_code=400, detail="La partida ya esta en juego")
+    
+    db.commit()
+    db.refresh(partida)
+
+    return {"mensaje":"La partida comenzo","estado": partida.estado}
+
+
+@partida_router.post("/partidas/{partida_id}/unirse", status_code= status.HTTP_201_CREATED)
+def unirse_a_partida(partida_id: int,jugador: JugadorSchema , db: Session = Depends(get_db)):
+    partida = db.query(PartidaModel).filter(PartidaModel.id_partida == partida_id).first()
+    if not partida:
+        raise HTTPException(status_code=404, detail="Partida no encontrada")
+
+    # Validar máximo de jugadores
+    if partida.cantidad_jugadores >= partida.maximo:
+        raise HTTPException(status_code=400, detail="La partida ya tiene el máximo de jugadores")
+
+    nuevo_jugador = JugadorModel(
+        id_partida=partida.id_partida,
+        nombre=jugador.nombre,
+        fecha_nacimiento=jugador.fecha_nacimiento,
+        orden_turno=0,
+        id_avatar=1  # ejemplo, puedes cambiar
+    )
+
+    db.add(nuevo_jugador)
+    partida.cantidad_jugadores +=1
+    db.commit()
+    db.refresh(nuevo_jugador)
+    db.refresh(partida)
+    return {
+        "mensaje":"jugador agregado",
+        "jugador_id":nuevo_jugador.id_jugador
+    }
+    
+
+
