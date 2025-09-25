6925a86932f05692acc91699120d80e88fb33244
DiogePuig <diogenes.puig@mi.unc.edu.ar>
2025-09-25 18:31:13 -0300

    SCRUM-9: agregado modelo de carta

---
+++ app/db/models/cartas_models.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Integer, ForeignKey, Enum, create_engine
+from sqlalchemy.orm import declarative_base, relationship
+import enum
+
+Base = declarative_base()
+
+class PosicionCarta(enum.Enum):
+    mazo = "mazo"          # Mazo normal
+    mano = "mano"          # Mano del jugador
+    descarte = "descarte"  # Mazo de descarte
+
+class Carta(Base):
+    __tablename__ = "cartas"
+
+    id_carta = Column(Integer, primary_key=True, autoincrement=True)
+    id_partida = Column(Integer, ForeignKey("partidas.id_partida"), nullable=False)
+    id_jugador = Column(Integer, ForeignKey("jugadores.id_jugador"), nullable=True)  # Puede no tener dueño
+
+    posicion = Column(Enum(PosicionCarta), nullable=False, default=PosicionCarta.mazo)
+
+    # Relaciones
+    jugador = relationship("Jugador", back_populates="cartas")
+    partida = relationship("Partida", back_populates="cartas")
+
+
