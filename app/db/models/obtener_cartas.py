5eba3ce61ad505a006235ede6cf4933cd2f82f4a
Linux User <franco.aredes@mi.unc.edu.ar>
2025-09-27 23:47:00 +0000

    SCRUM-20 FIX agregado import a obtener_cartas

--- app/db/models/obtener_cartas.py
+++ app/db/models/obtener_cartas.py
@@ -2,7 +2,7 @@
 from fastapi import WebSocket
 from sqlalchemy.orm import Session
 import random
-from typing import List, Dict
+from typing import List, Dict, Any
 
 async def repartir_cartas_a_jugadores(db: Session, partida_id: int, num_cartas: int) -> Dict[str, Any]:
 
