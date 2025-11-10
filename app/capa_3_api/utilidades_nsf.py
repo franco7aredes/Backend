f3481bb9522f751b79af842452784a81699e8549
Franco <franco.aredes@mi.unc.edu.ar>
2025-11-10 14:52:34 -0300

    SCRUM-139 ADD hechas nuevas funciones para limpiar routers

--- app/capa_3_api/utilidades_nsf.py
+++ app/capa_3_api/utilidades_nsf.py
@@ -1,6 +1,6 @@
 import asyncio
 import time
-from typing import Dict
+from typing import Dict, Any
 from .nsf_tipos import VentanaNSFActiva
 from .websockets.ApiWS import administrador
 
@@ -37,3 +37,54 @@ async def gestionar_fin_ventana(
         await administrador.difundir_a_partida(actual.partida_id, mensaje)
     finally:
         ventanas_dict.pop(actual.partida_id, None)
+
+async def activar_ventana_nsf(
+    tipo_accion: str, 
+    id_jugador: int, 
+    partida_id: int,
+    ventanas_dict: Dict[int, VentanaNSFActiva],
+    payload: Dict[str, Any]
+) -> VentanaNSFActiva:
+
+    # creamos la ventana
+
+    ventana_id = uuid.uuid4().hex
+    tiempo = await tiempo_en_ms(5.0)
+
+    ventana = VentanaNSFActiva(
+        partida_id=partida_id,
+        ventana_id=ventana_id,
+        actor_id=id_jugador,
+        tipo_accion=tipo_accion,
+        payload=payload,
+        contador=0,
+        tiempo_ms=tiempo,
+    )
+    _VENTANAS[partida_id] = ventana
+
+    # orquestamos la tarea
+    ventana.tarea = asyncio.create_task(
+        gestionar_fin_ventana(ventana, _VENTANAS)
+    )
+
+    return ventana
+
+async def refrescar_ventana_nsf(ventana: VentanaNSFActiva) -> None:
+
+        # orquestamos la tarea
+    ventana.contador += 1
+    ventana.tiempo_ms = await tiempo_en_ms(5.0) # renuevo el timer
+
+    # reprogramo la resolucion de la ventana
+    if ventana.tarea and not ventana.tarea.done():
+        ventana.tarea.cancel()
+        try:
+            await asyncio.sleep(0) # permito que se procese la cancelacion
+        except Exception:
+            pass
+
+    ventana.tarea = asyncio.create_task(
+        gestionar_fin_ventana(ventana, _VENTANAS)
+    )
+
+
